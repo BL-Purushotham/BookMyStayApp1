ddc952538428718931444d9a9f9bad5a8b998b83
BL-Purushotham <purushotham.cirimala@bridgelabz.com>
2026-06-20 10:20:28 +0530

    UC2 Search Service

--- src/com/bridgelabz/bookmystayapp/BookMyStayApp.java
+++ src/com/bridgelabz/bookmystayapp/BookMyStayApp.java
@@ -18,6 +18,24 @@ public class BookMyStayApp {
 
         System.out.println("Suite Availability : "
                 + inventory.getAvailableRooms("Suite"));
+<<<<<<< HEAD
+=======
+
+        System.out.println("===== SEARCHING ROOMS =====");
+
+        SearchService searchService =
+                new SearchService(inventory);
+
+        System.out.println("Searching Single Room");
+
+        searchService.searchRoom("Single");
+
+        System.out.println();
+
+        System.out.println("Searching Double Room");
+
+        searchService.searchRoom("Double");
+>>>>>>> feature/UC2-SearchSerive
     }
 
 }
